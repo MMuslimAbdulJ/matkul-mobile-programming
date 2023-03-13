15aeb4a8a5989bda631fdfd510b1c0a9068587eb
Muhammad Muslim Abdul Jabbaar <mmuslimabdulj@gmail.com>
2023-03-13 11:03:49 +0700

    initial commit

---
+++ Pertemuan4/app/src/main/java/com/example/pertemuan4/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.pertemuan4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.table_layout)
+    }
+}
